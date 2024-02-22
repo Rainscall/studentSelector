51f8201935537c477bb9aa33d6fca8bc39f0968d
Rainscall <admin@typeboom.com>
2024-02-22 22:18:35 +0800

    modified:   src/script.js 	modified:   src/style.css

--- src/script.js
+++ src/script.js
@@ -111,6 +111,15 @@ function init() {
                 img.classList.add('img');
                 img.style.backgroundImage = `url('${picList[selectedCharacter[i]]}')`;
 
+                base.addEventListener('click', () => {
+                    selectedCharacter = selectedCharacter.filter(item => item !== selectedCharacter[i]);
+                    if (selectedCharacter.length === 0) {//处理角色被删完的情况
+                        init();
+                        return;
+                    }
+                    base.parentNode.removeChild(base);
+                });
+
                 base.appendChild(img);
                 base.appendChild(span);
                 value.appendChild(base);
@@ -136,12 +145,16 @@ async function openCharacterList() {
                 'name': '返回',
                 'iconSvg': '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>',
                 'callback': 'init'
-            }
-            ,
+            },
             {
                 'name': '清空',
                 'iconSvg': '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>',
                 'callback': 'clearSelectedCharacter'
+            },
+            {
+                'name': '顶部',
+                'iconSvg': '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"/></svg>',
+                'callback': 'backToTop'
             }
         ]
         let base = document.createElement('div');
@@ -380,6 +393,12 @@ function clearSelectedCharacter() {
     }
     selectedCharacter = [];
     createToast('清除完成', 2640);
+}
+
+function backToTop() {
+    infoArea.children[0].scrollIntoView({
+        behavior: "smooth"
+    });
 }
 
 const picList = {
