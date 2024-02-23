47c1cfff0405f05c25b56603182a1807db0b21b7
Rainscall <admin@typeboom.com>
2024-02-23 10:12:49 +0800

    modified:   src/script.js 	modified:   src/style.css

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 "use strict"
-const apiEndpoint = 'https://studentselector-backend.colorspark.net/';
+const apiEndpoint = 'https://studentselector-backend.colorspark.net';
 let selectedCharacter = [];
 
 function init() {
@@ -97,10 +97,39 @@ function init() {
             infoArea.appendChild(document.createElement('hr'));
             let base = document.createElement('div');
             let title = document.createElement('div');
+            let titleText = document.createElement('span');
+            let titleIcon = document.createElement('div');
             let value = document.createElement('div');
 
             base.classList.add('selectedCharacterList');
-            title.innerText = '已选择以下角色';
+            titleText.innerText = '已选择以下角色';
+            titleIcon.classList.add('inlineSvgIcon');
+            titleIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>';
+            title.appendChild(titleText);
+            title.appendChild(titleIcon);
+
+            titleIcon.addEventListener('click', () => {
+                if (selectedCharacter.length === 0) {
+                    createToast('至少需要选择一个角色', 2540, '#FFF', '#414141', 'temp-search-loadingToast');
+                    return;
+                }
+                let queryStr = '';
+                for (let i = 0; i < selectedCharacter.length; i++) {
+                    let str = `${selectedCharacter[i]}`;
+                    if (selectedCharacter.length > 1 && i != selectedCharacter.length - 1) {
+                        str += ',';
+                    }
+                    queryStr += str;
+                }
+                createToast('正在查询', -1, '#FFF', '#414141', 'temp-search-loadingToast');
+                doSearch(queryStr, 'things')
+                    .then(r => {
+                        if (r == 'NOT FOUND') {
+                            createToast('未找到符合条件的项目', 4300, '#FFF', '#840D23');
+                            input.value = '';
+                        }
+                    });
+            });
 
             for (let i = 0; i < selectedCharacter.length; i++) {
                 let base = document.createElement('div');
