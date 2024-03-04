bd9bdd0a18caef9171b3117c8968be3713f0cc84
Rainscall <admin@typeboom.com>
2024-03-04 23:31:58 +0800

    modified:   _headers 	modified:   index.html 	new file:   src/img/picList.json 	new file:   src/json/aliasList.json 	modified:   src/script.js

--- src/script.js
+++ src/script.js
@@ -2,9 +2,11 @@
 const apiEndpoint = 'https://backend.fgopy.com';
 let selectedCharacter = [];
 let netCache = {
-    respondCache: undefined,
+    respondCache: null,
     pageCache: [],
-    lastStartsFrom: 0
+    lastStartsFrom: 0,
+    picList: null,
+    aliasList: null
 };
 let sortByCache = '';
 const stepLength = 25;
@@ -204,7 +206,7 @@ function init() {
         infoArea.appendChild(base);
     })();
 
-    (() => {
+    (async () => {
         if (selectedCharacter.length > 0) {
             infoArea.appendChild(document.createElement('hr'));
             let base = document.createElement('div');
@@ -219,6 +221,14 @@ function init() {
             titleIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>';
             title.appendChild(titleText);
             title.appendChild(titleIcon);
+
+            let picList;
+            if (netCache.picList == null) {
+                picList = await fetch('src/img/picList.json').then(r => r.json());
+                netCache.picList = picList;
+            } else {
+                picList = netCache.picList;
+            }
 
             titleIcon.addEventListener('click', () => {
                 if (selectedCharacter.length === 0) {
@@ -821,6 +831,14 @@ async function openCharacterList() {
     blankDiv.style.height = '4.2rem';
     infoArea.appendChild(blankDiv);
 
+    let picList;
+    if (netCache.picList == null) {
+        picList = await fetch('src/img/picList.json').then(r => r.json());
+        netCache.picList = picList;
+    } else {
+        picList = netCache.picList;
+    }
+
     for (let i = 0; i < Object.keys(picList).length; i++) {
         const infoList = picList;
         const infoArea = document.getElementById('infoArea');
@@ -1006,6 +1024,14 @@ async function writeInfo(r, sortOrder = 'asc', sortBy = 'coins', maxPageSize = s
 
             value.dataset.value = displayValue;
 
+            let picList;
+            if (netCache.picList == null) {
+                picList = await fetch('src/img/picList.json').then(r => r.json());
+                netCache.picList = picList;
+            } else {
+                picList = netCache.picList;
+            }
+
             switch (currentKey) {
                 case 'id': {
                     base.addEventListener('click', () => {
@@ -1163,8 +1189,8 @@ async function doSearch(query = '1.1.1.1', type) {
             let queryList = query.split(',');
             query = '';
             for (let i = 0; i < queryList.length; i++) {
-                if (aliasToName(queryList[i]) !== false) {
-                    queryList[i] = aliasToName(queryList[i]);
+                if (await aliasToName(queryList[i]) !== false) {
+                    queryList[i] = await aliasToName(queryList[i]);
                     showRedircted = true;
                 }
                 query += queryList[i];
@@ -1175,8 +1201,8 @@ async function doSearch(query = '1.1.1.1', type) {
             if (showRedircted) {
                 createToast(`重定向至 ${query}`);
             }
-        } else if (aliasToName(query) !== false) {
-            query = aliasToName(query);
+        } else if (await aliasToName(query) !== false) {
+            query = await aliasToName(query);
             createToast(`重定向至 ${query}`);
         }
 
@@ -1356,8 +1382,17 @@ function getAverageRGB(imgEl) {
  * @param {String} input 
  * @returns 
  */
-function aliasToName(input) {
+async function aliasToName(input) {
     input = input.toLowerCase();
+    let aliasList;
+
+    if (netCache.aliasList == null) {
+        aliasList = await fetch('src/json/aliasList.json').then(r => r.json());
+        netCache.aliasList = aliasList;
+    } else {
+        aliasList = netCache.aliasList;
+    }
+
     for (let i = 0; i < Object.keys(aliasList).length; i++) {
         if (Object.values(aliasList)[i].includes(input)) {
             return Object.keys(aliasList)[i];
@@ -1462,704 +1497,6 @@ function openSortMenu() {
     dialog.appendChild(base);
     document.body.appendChild(dialog);
     dialog.showModal();
-}
-
-const picList = {
-    "阿尔托莉雅・卡斯特": "./src/img/heroes/Servant284.jpg",
-    "雨之魔女梣": "./src/img/heroes/Servant385.jpg",
-    "阿瓦隆女士": "./src/img/heroes/Servant353.jpg",
-    "Archetype：Earth": "./src/img/heroes/Servant351.jpg",
-    "奥伯龙": "./src/img/heroes/Servant316.jpg",
-    "光之高扬斯卡娅": "./src/img/heroes/Servant314.jpg",
-    "妖精骑士兰斯洛特": "./src/img/heroes/Servant312.jpg",
-    "摩根": "./src/img/heroes/Servant309.jpg",
-    "梅林": "./src/img/heroes/Servant150.jpg",
-    "诸葛孔明〔埃尔梅罗Ⅱ世〕": "./src/img/heroes/Servant037.jpg",
-    "安德洛墨达": "./src/img/heroes/Servant406.jpg",
-    "源赖光／丑御前": "./src/img/heroes/Servant403.jpg",
-    "日本武尊": "./src/img/heroes/Servant402.jpg",
-    "上杉谦信": "./src/img/heroes/Servant400.jpg",
-    "武田晴信": "./src/img/heroes/Servant397.jpg",
-    "托勒密": "./src/img/heroes/Servant394.jpg",
-    "旺吉娜": "./src/img/heroes/Servant393.jpg",
-    "梅柳齐娜": "./src/img/heroes/Servant390.jpg",
-    "美杜莎": "./src/img/heroes/Servant384.jpg",
-    "杜尔伽": "./src/img/heroes/Servant383.jpg",
-    "怖军": "./src/img/heroes/Servant381.jpg",
-    "果心居士": "./src/img/heroes/Servant380.jpg",
-    "所多玛之兽／德拉科": "./src/img/heroes/Servant377.jpg",
-    "幼体／提亚马特": "./src/img/heroes/Servant376.jpg",
-    "高杉晋作": "./src/img/heroes/Servant375.jpg",
-    "女教皇若安": "./src/img/heroes/Servant374.jpg",
-    "库库尔坎": "./src/img/heroes/Servant373.jpg",
-    "特斯卡特利波卡": "./src/img/heroes/Servant371.jpg",
-    "尼托克丽丝〔Alter〕": "./src/img/heroes/Servant370.jpg",
-    "格里戈里・拉斯普京": "./src/img/heroes/Servant369.jpg",
-    "布里托玛特": "./src/img/heroes/Servant368.jpg",
-    "呼延灼": "./src/img/heroes/Servant365.jpg",
-    "千利休": "./src/img/heroes/Servant362.jpg",
-    "斯卡哈・斯卡蒂": "./src/img/heroes/Servant215.jpg",
-    "伊吹童子": "./src/img/heroes/Servant299.jpg",
-    "源为朝": "./src/img/heroes/Servant350.jpg",
-    "曲亭马琴": "./src/img/heroes/Servant349.jpg",
-    "詹姆斯・莫里亚蒂": "./src/img/heroes/Servant346.jpg",
-    "查理曼": "./src/img/heroes/Servant343.jpg",
-    "君士坦丁十一世": "./src/img/heroes/Servant342.jpg",
-    "超级班扬": "./src/img/heroes/Servant339.jpg",
-    "征氏姐妹": "./src/img/heroes/Servant337.jpg",
-    "马纳南・麦克・利尔〔巴泽特〕": "./src/img/heroes/Servant336.jpg",
-    "暗之高扬斯卡娅": "./src/img/heroes/Servant334.jpg",
-    "BeastⅣ": "./src/img/heroes/Servant333.jpg",
-    "太公望": "./src/img/heroes/Servant331.jpg",
-    "坂本龙马": "./src/img/heroes/Servant329.jpg",
-    "出云阿国": "./src/img/heroes/Servant327.jpg",
-    "雅克・德・莫莱": "./src/img/heroes/Servant324.jpg",
-    "迦摩": "./src/img/heroes/Servant239.jpg",
-    "冲田总司〔Alter〕": "./src/img/heroes/Servant209.jpg",
-    "克莱恩小姐": "./src/img/heroes/Servant307.jpg",
-    "伽拉忒亚": "./src/img/heroes/Servant306.jpg",
-    "阿摩耳〔卡莲〕": "./src/img/heroes/Servant305.jpg",
-    "平景清": "./src/img/heroes/Servant303.jpg",
-    "千子村正": "./src/img/heroes/Servant302.jpg",
-    "弗栗多": "./src/img/heroes/Servant300.jpg",
-    "芦屋道满": "./src/img/heroes/Servant297.jpg",
-    "尼莫": "./src/img/heroes/Servant296.jpg",
-    "梵高": "./src/img/heroes/Servant295.jpg",
-    "卑弥呼": "./src/img/heroes/Servant292.jpg",
-    "阿比盖尔・威廉姆斯〔夏〕": "./src/img/heroes/Servant289.jpg",
-    "杀生院祈荒": "./src/img/heroes/Servant167.jpg",
-    "旅行者": "./src/img/heroes/Servant281.jpg",
-    "罗穆路斯・奎里努斯": "./src/img/heroes/Servant280.jpg",
-    "狄俄斯库里": "./src/img/heroes/Servant278.jpg",
-    "奥德修斯": "./src/img/heroes/Servant277.jpg",
-    "清少纳言": "./src/img/heroes/Servant276.jpg",
-    "杨贵妃": "./src/img/heroes/Servant275.jpg",
-    "欧罗巴": "./src/img/heroes/Servant274.jpg",
-    "超人俄里翁": "./src/img/heroes/Servant272.jpg",
-    "阿斯托尔福": "./src/img/heroes/Servant270.jpg",
-    "太空伊什塔尔": "./src/img/heroes/Servant268.jpg",
-    "阿尔托莉雅・潘德拉贡": "./src/img/heroes/Servant002.jpg",
-    "宫本武藏": "./src/img/heroes/Servant153.jpg",
-    "莱昂纳多・达・芬奇": "./src/img/heroes/Servant127.jpg",
-    "织田信长": "./src/img/heroes/Servant250.jpg",
-    "阿周那〔Alter〕": "./src/img/heroes/Servant247.jpg",
-    "伟大的石像神": "./src/img/heroes/Servant244.jpg",
-    "司马懿〔莱妮丝〕": "./src/img/heroes/Servant241.jpg",
-    "BeastⅢ／L": "./src/img/heroes/Servant240.jpg",
-    "Kingprotea": "./src/img/heroes/Servant238.jpg",
-    "紫式部": "./src/img/heroes/Servant237.jpg",
-    "李书文": "./src/img/heroes/Servant235.jpg",
-    "红阎魔": "./src/img/heroes/Servant234.jpg",
-    "布拉达曼特": "./src/img/heroes/Servant232.jpg",
-    "始皇帝": "./src/img/heroes/Servant229.jpg",
-    "项羽": "./src/img/heroes/Servant226.jpg",
-    "志度内": "./src/img/heroes/Servant224.jpg",
-    "BB": "./src/img/heroes/Servant220.jpg",
-    "贞德": "./src/img/heroes/Servant059.jpg",
-    "齐格鲁德": "./src/img/heroes/Servant213.jpg",
-    "拿破仑": "./src/img/heroes/Servant212.jpg",
-    "阿喀琉斯": "./src/img/heroes/Servant206.jpg",
-    "伊凡雷帝": "./src/img/heroes/Servant205.jpg",
-    "阿纳斯塔西娅": "./src/img/heroes/Servant201.jpg",
-    "塞弥拉弥斯": "./src/img/heroes/Servant199.jpg",
-    "葛饰北斋": "./src/img/heroes/Servant198.jpg",
-    "埃列什基伽勒": "./src/img/heroes/Servant196.jpg",
-    "阿比盖尔・威廉姆斯": "./src/img/heroes/Servant195.jpg",
-    "刑部姬": "./src/img/heroes/Servant189.jpg",
-    "阿尔托莉雅・潘德拉贡〔Alter〕": "./src/img/heroes/Servant179.jpg",
-    "尼禄・克劳狄乌斯": "./src/img/heroes/Servant175.jpg",
-    "夏洛克・福尔摩斯": "./src/img/heroes/Servant173.jpg",
-    "不夜城的Caster": "./src/img/heroes/Servant169.jpg",
-    "BeastⅢ／R": "./src/img/heroes/Servant168.jpg",
-    "Meltryllis": "./src/img/heroes/Servant163.jpg",
-    "土方岁三": "./src/img/heroes/Servant161.jpg",
-    "亚瑟・潘德拉贡〔Prototype〕": "./src/img/heroes/Servant160.jpg",
-    "新宿的Archer": "./src/img/heroes/Servant156.jpg",
-    "谜之女主角X〔Alter〕": "./src/img/heroes/Servant155.jpg",
-    "“山中老人”": "./src/img/heroes/Servant154.jpg",
-    "所罗门": "./src/img/heroes/Servant083.jpg",
-    "盖提亚": "./src/img/heroes/Servant151.jpg",
-    "提亚马特": "./src/img/heroes/Servant149.jpg",
-    "魁札尔・科亚特尔": "./src/img/heroes/Servant144.jpg",
-    "恩奇都": "./src/img/heroes/Servant143.jpg",
-    "伊什塔尔": "./src/img/heroes/Servant142.jpg",
-    "克娄巴特拉": "./src/img/heroes/Servant139.jpg",
-    "伊莉雅丝菲尔・冯・爱因兹贝伦": "./src/img/heroes/Servant136.jpg",
-    "玉藻前": "./src/img/heroes/Servant062.jpg",
-    "奥斯曼狄斯": "./src/img/heroes/Servant118.jpg",
-    "源赖光": "./src/img/heroes/Servant114.jpg",
-    "玄奘三藏": "./src/img/heroes/Servant113.jpg",
-    "酒吞童子": "./src/img/heroes/Servant112.jpg",
-    "伊斯坎达尔": "./src/img/heroes/Servant108.jpg",
-    "贞德〔Alter〕": "./src/img/heroes/Servant106.jpg",
-    "女王梅芙": "./src/img/heroes/Servant099.jpg",
-    "库・丘林〔Alter〕": "./src/img/heroes/Servant098.jpg",
-    "南丁格尔": "./src/img/heroes/Servant097.jpg",
-    "岩窟王": "./src/img/heroes/Servant096.jpg",
-    "天草四郎": "./src/img/heroes/Servant093.jpg",
-    "两仪式": "./src/img/heroes/Servant091.jpg",
-    "尼禄・克劳狄乌斯〔新娘〕": "./src/img/heroes/Servant090.jpg",
-    "布伦希尔德": "./src/img/heroes/Servant088.jpg",
-    "谜之女主角X": "./src/img/heroes/Servant086.jpg",
-    "迦尔纳": "./src/img/heroes/Servant085.jpg",
-    "阿周那": "./src/img/heroes/Servant084.jpg",
-    "尼古拉・特斯拉": "./src/img/heroes/Servant077.jpg",
-    "莫德雷德": "./src/img/heroes/Servant076.jpg",
-    "开膛手杰克": "./src/img/heroes/Servant075.jpg",
-    "斯卡哈": "./src/img/heroes/Servant070.jpg",
-    "冲田总司": "./src/img/heroes/Servant068.jpg",
-    "弗朗西斯・德雷克": "./src/img/heroes/Servant065.jpg",
-    "俄里翁": "./src/img/heroes/Servant060.jpg",
-    "弗拉德三世": "./src/img/heroes/Servant052.jpg",
-    "坂田金时": "./src/img/heroes/Servant051.jpg",
-    "吉尔伽美什": "./src/img/heroes/Servant012.jpg",
-    "阿蒂拉": "./src/img/heroes/Servant008.jpg"
-}
-
-const aliasList = {
-    "安德洛墨达": [
-
-    ],
-    "源赖光／丑御前": [
-
-    ],
-    "日本武尊": [
-
-    ],
-    "上杉谦信": [
-
-    ],
-    "武田晴信": [
-
-    ],
-    "托勒密": [
-
-    ],
-    "旺吉娜": [
-
-    ],
-    "梅柳齐娜": [
-
-    ],
-    "阿尔托莉雅・卡斯特": [
-
-    ],
-    "雨之魔女梣": [
-
-    ],
-    "美杜莎": [
-        'r姐'
-    ],
-    "杜尔伽": [
-
-    ],
-    "怖军": [
-
-    ],
-    "果心居士": [
-
-    ],
-    "所多玛之兽／德拉科": [
-
-    ],
-    "幼体／提亚马特": [
-
-    ],
-    "高杉晋作": [
-
-    ],
-    "女教皇若安": [
-
-    ],
-    "库库尔坎": [
-
-    ],
-    "特斯卡特利波卡": [
-
-    ],
-    "尼托克丽丝〔Alter〕": [
-
-    ],
-    "格里戈里・拉斯普京": [
-
-    ],
-    "布里托玛特": [
-
-    ],
-    "呼延灼": [
-
-    ],
-    "千利休": [
-
-    ],
-    "斯卡哈・斯卡蒂": [
-        'cba'
-    ],
-    "伊吹童子": [
-
-    ],
-    "阿瓦隆女士": [
-        '梅利'
-    ],
-    "Archetype：Earth": [
-        '公主'
-    ],
-    "源为朝": [
-
-    ],
-    "曲亭马琴": [
-
-    ],
-    "詹姆斯・莫里亚蒂": [
-
-    ],
-    "查理曼": [
-
-    ],
-    "君士坦丁十一世": [
-
-    ],
-    "超级班扬": [
-
-    ],
-    "征氏姐妹": [
-
-    ],
-    "马纳南・麦克・利尔〔巴泽特〕": [
-
-    ],
-    "暗之高扬斯卡娅": [
-
-    ],
-    "BeastⅣ": [
-
-    ],
-    "太公望": [
-
-    ],
-    "坂本龙马": [
-
-    ],
-    "出云阿国": [
-
-    ],
-    "雅克・德・莫莱": [
-
-    ],
-    "迦摩": [
-
-    ],
-    "冲田总司〔Alter〕": [
-
-    ],
-    "奥伯龙": [
-        '杀狐'
-    ],
-    "光之高扬斯卡娅": [
-
-    ],
-    "妖精骑士兰斯洛特": [
-
-    ],
-    "摩根": [
-
-    ],
-    "克莱恩小姐": [
-
-    ],
-    "伽拉忒亚": [
-
-    ],
-    "阿摩耳〔卡莲〕": [
-
-    ],
-    "平景清": [
-
-    ],
-    "千子村正": [
-
-    ],
-    "弗栗多": [
-
-    ],
-    "芦屋道满": [
-
-    ],
-    "尼莫": [
-
-    ],
-    "梵高": [
-
-    ],
-    "卑弥呼": [
-
-    ],
-    "阿比盖尔・威廉姆斯〔夏〕": [
-
-    ],
-    "杀生院祈荒": [
-
-    ],
-    "旅行者": [
-
-    ],
-    "罗穆路斯・奎里努斯": [
-
-    ],
-    "狄俄斯库里": [
-
-    ],
-    "奥德修斯": [
-
-    ],
-    "清少纳言": [
-
-    ],
-    "杨贵妃": [
-
-    ],
-    "欧罗巴": [
-
-    ],
-    "超人俄里翁": [
-
-    ],
-    "阿斯托尔福": [
-        '阿福',
-        'fa真女主'
-    ],
-    "太空伊什塔尔": [
-
-    ],
-    "阿尔托莉雅・潘德拉贡": [
-        '呆毛王'
-    ],
-    "宫本武藏": [
-
-    ],
-    "莱昂纳多・达・芬奇": [
-        '大碧池',
-        '大碧池酱',
-        '碧池a梦'
-    ],
-    "织田信长": [
-        '信长',
-        'nobu',
-        '钉宫',
-        '型月钉宫病原菌',
-        '第六天萌王',
-        'luo体披风',
-        '裸体披风'
-    ],
-    "阿周那〔Alter〕": [
-
-    ],
-    "伟大的石像神": [
-
-    ],
-    "司马懿〔莱妮丝〕": [
-
-    ],
-    "BeastⅢ／L": [
-
-    ],
-    "Kingprotea": [
-
-    ],
-    "紫式部": [
-
-    ],
-    "李书文": [
-
-    ],
-    "红阎魔": [
-
-    ],
-    "布拉达曼特": [
-
-    ],
-    "始皇帝": [
-
-    ],
-    "项羽": [
-
-    ],
-    "志度内": [
-
-    ],
-    "BB": [
-
-    ],
-    "贞德": [
-        '村姑',
-        '尺子',
-        '贞日天',
-        '法国民心'
-    ],
-    "齐格鲁德": [
-
-    ],
-    "拿破仑": [
-
-    ],
-    "阿喀琉斯": [
-
-    ],
-    "伊凡雷帝": [
-
-    ],
-    "阿纳斯塔西娅": [
-
-    ],
-    "塞弥拉弥斯": [
-
-    ],
-    "葛饰北斋": [
-
-    ],
-    "埃列什基伽勒": [
-
-    ],
-    "阿比盖尔・威廉姆斯": [
-
-    ],
-    "刑部姬": [
-
-    ],
-    "阿尔托莉雅・潘德拉贡〔Alter〕": [
-        '黑呆'
-    ],
-    "尼禄・克劳狄乌斯": [
-        '暴君',
-        '尼禄',
-        '红saber'
-    ],
-    "夏洛克・福尔摩斯": [
-
-    ],
-    "不夜城的Caster": [
-
-    ],
-    "BeastⅢ／R": [
-        'beastⅢ/r',
-        'beastiii/r'
-    ],
-    "Meltryllis": [
-
-    ],
-    "土方岁三": [
-
-    ],
-    "亚瑟・潘德拉贡〔Prototype〕": [
-
-    ],
-    "新宿的Archer": [
-
-    ],
-    "谜之女主角X〔Alter〕": [
-
-    ],
-    "“山中老人”": [
-        '"山中老人"',
-        '"山中老头"',
-        '山中老人'
-    ],
-    "所罗门": [
-        'poi',
-        '小便王'
-    ],
-    "盖提亚": [
-
-    ],
-    "梅林": [
-        '变态'
-    ],
-    "提亚马特": [
-
-    ],
-    "魁札尔・科亚特尔": [
-
-    ],
-    "恩奇都": [
-
-    ],
-    "伊什塔尔": [
-
-    ],
-    "克娄巴特拉": [
-
-    ],
-    "伊莉雅丝菲尔・冯・爱因兹贝伦": [
-        '伊莉雅'
-    ],
-    "玉藻前": [
-        '小玉',
-        'c玉',
-        'c狐'
-    ],
-    "奥斯曼狄斯": [
-
-    ],
-    "源赖光": [
-        '奶光妈妈',
-        '奶光'
-    ],
-    "玄奘三藏": [
-        '三藏',
-        '女子高僧',
-        'jk高僧',
-        '说教',
-        '替身使者',
-        '打拳三人组'
-    ],
-    "酒吞童子": [
-        '酒吞',
-        'umb',
-        '凹酱',
-        '西瓜'
-    ],
-    "伊斯坎达尔": [
-        '大帝'
-    ],
-    "贞德〔Alter〕": [
-
-    ],
-    "女王梅芙": [
-        '梅芙',
-        '妹夫',
-        '碧池',
-        '老司机',
-        '狗控',
-        '梅芙暖暖'
-    ],
-    "库・丘林〔Alter〕": [
-
-    ],
-    "南丁格尔": [
-        '南丁',
-        '蔻蔻',
-        '护士'
-    ],
-    "岩窟王": [
-
-    ],
-    "天草四郎": [
-
-    ],
-    "两仪式": [
-
-    ],
-    "尼禄・克劳狄乌斯〔新娘〕": [
-        '嫁王',
-        '嫁尼禄',
-        '白色暴君',
-        '花嫁'
-    ],
-    "布伦希尔德": [
-        '布姐(蓝)',
-        '病娇枪',
-        '笑什么笑，你也是西格鲁特',
-        '西嫂',
-        '飞嫂'
-    ],
-    "谜之女主角X": [
-        'x毛',
-        '杀手',
-        '桐子',
-        '星爆弃疗斩',
-        '插拔',
-        '六星特效'
-    ],
-    "迦尔纳": [
-        '小太阳',
-        '阿三(白)',
-        'launcher',
-        '真英雄',
-        '大兄'
-    ],
-    "阿周那": [
-        'jojo那',
-        'jojo',
-        '周周那',
-        '阿三(黑)',
-        '滑稽那',
-        '娜娜子',
-        '周黑鸭',
-        '阿囧',
-        '阿囧那',
-        '倒射弓'
-    ],
-    "尼古拉・特斯拉": [
-        '祖爷爷',
-        '特总'
-    ],
-    "莫德雷德": [
-        '小莫',
-        '莫受',
-        '熊孩子',
-        '坑爹剑',
-        '逆子',
-        '父控'
-    ],
-    "开膛手杰克": [
-
-    ],
-    "斯卡哈": [
-        '师匠',
-        '西秀'
-    ],
-    "冲田总司": [
-        '樱saber',
-        '总司',
-        '冲田',
-        '对斯卡哈人型宝具',
-        'saber(assassin)',
-        '病弱'
-    ],
-    "弗朗西斯・德雷克": [
-        '船长',
-        '德姐'
-    ],
-    "俄里翁": [
-
-    ],
-    "弗拉德三世": [
-        '大公',
-        '穿刺公',
-        '刺绣公',
-        '四星枪兵',
-        '五星之耻'
-    ],
-    "坂田金时": [
-        '金时',
-        '狗蛋',
-        '金良辰',
-        '金爸爸',
-        'r金时',
-        '暴走族',
-        '假面骑士金时',
-        '秋名山车神'
-    ],
-    "诸葛孔明〔埃尔梅罗Ⅱ世〕": [
-        '王妃',
-        '孔日天',
-        '六星',
-        '村夫',
-        '老虚亲儿子'
-    ],
-    "吉尔伽美什": [
-        '金闪闪',
-        'C闪',
-        '闪闪',
-        '二闪',
-        '金皮卡',
-        '路灯王',
-        '水枪王',
-        'aup'
-    ],
-    "阿蒂拉": [
-        '大王'
-    ]
 }
 
 console.log(`%c ${languageAssets.others.consoleWarnMessage}`, 'color: red; font-size: xx-large; font-family: Arial, Helvetica, sans-serif; background-color: yellow;');
